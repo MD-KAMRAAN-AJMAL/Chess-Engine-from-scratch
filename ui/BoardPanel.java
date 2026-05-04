7b8e3c55111c73876c5b490f335d3438b41ea492
Md Kamraan Ajmal <kamranajmal7786@gmail.com>
2026-05-05 00:38:51 +0530

    Add Piece Model and Individual piece highlight and Moved Piece enum to separate file

--- ui/BoardPanel.java
+++ ui/BoardPanel.java
@@ -3,25 +3,57 @@ package ui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 import models.BoardModel;
+import models.Piece;
 
 public class BoardPanel extends JPanel {
 
     private final int squareSize = 75;
 
+    private Piece selectedPiece;
+
     public BoardPanel() {
         setPreferredSize(new Dimension(600, 600));
+        addMouseListener(
+            new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    int col = (Integer) (e.getX() / squareSize);
+                    int row = (Integer) (e.getY() / squareSize);
+
+                    selectedPiece = BoardModel.board[row][col];
+
+                    repaint();
+                }
+            }
+        );
     }
 
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        final var board = BoardModel.board;
+        drawBoard(g);
+        drawHightlight(g);
+        drawPieces(g);
+    }
 
+    private void drawHightlight(Graphics g) {
+        if (selectedPiece != null) {
+            int selectedCol = selectedPiece.getPosX();
+            int selectedRow = selectedPiece.getPosY();
+
+            g.setColor(new Color(255, 255, 0, 120));
+            g.fillRect(selectedCol, selectedRow, squareSize, squareSize);
+        }
+    }
+
+    private void drawBoard(Graphics g) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                // draw board
                 if ((i + j) % 2 == 0) {
                     g.setColor(Color.WHITE);
                 } else {
@@ -33,14 +65,19 @@ public class BoardPanel extends JPanel {
                     squareSize,
                     squareSize
                 );
+            }
+        }
+    }
 
+    private void drawPieces(Graphics g) {
+        var board = BoardModel.board;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 var piece = board[i][j];
-
-                // draw pieces
                 if (piece != null) {
                     piece.setPos(squareSize * j, squareSize * i);
                     g.drawImage(
-                        piece.getImage(),
+                        piece.getType().getImage(),
                         piece.getPosX(),
                         piece.getPosY(),
                         squareSize,
