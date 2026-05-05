bac2a5afa6e1ea01b386a1eb0d676b1168add81a
Md Kamraan Ajmal <kamranajmal7786@gmail.com>
2026-05-05 12:17:17 +0530

    Add piece values to PieceType and remove position

--- models/PieceType.java
+++ models/PieceType.java
@@ -4,49 +4,51 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public enum PieceType {
-    PAWN("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-pawn.png"),
-    ROOK("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-rook.png"),
+    PAWN("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-pawn.png", 1),
+    ROOK("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-rook.png", 5),
     BISHOP(
-        "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-bishop.png"
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-bishop.png",
+        3
     ),
     KNIGHT(
-        "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-knight.png"
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-knight.png",
+        3
     ),
-    QUEEN("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-queen.png"),
-    KING("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-king.png"),
-    pawn("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-pawn.png"),
-    rook("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-rook.png"),
+    QUEEN(
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-queen.png",
+        9
+    ),
+    KING("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-king.png", 0),
+    pawn("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-pawn.png", 1),
+    rook("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-rook.png", 5),
     bishop(
-        "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-bishop.png"
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-bishop.png",
+        3
     ),
     knight(
-        "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-knight.png"
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-knight.png",
+        3
     ),
-    queen("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-queen.png"),
-    king("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-king.png");
+    queen(
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-queen.png",
+        9
+    ),
+    king("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-king.png", 0);
 
     private Image pImage = null;
-    private int row;
-    private int col;
 
-    private PieceType(String imagePath) {
-        pImage = new ImageIcon(imagePath).getImage();
+    private int value;
+
+    private PieceType(String imagePath, int value) {
+        this.pImage = new ImageIcon(imagePath).getImage();
+        this.value = value;
     }
 
     public Image getImage() {
         return pImage;
     }
 
-    public int getRow() {
-        return row;
-    }
-
-    public void setPos(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-
-    public int getCol() {
-        return col;
+    public int value() {
+        return value;
     }
 }
