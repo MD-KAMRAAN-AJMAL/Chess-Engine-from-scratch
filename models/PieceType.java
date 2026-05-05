97946f0b7184acdf5e4046b1d2f598f5f8fa52c1
Md Kamraan Ajmal <kamranajmal7786@gmail.com>
2026-05-05 13:37:05 +0530

    Add isWhite flag to PieceType enum

--- models/PieceType.java
+++ models/PieceType.java
@@ -4,44 +4,76 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public enum PieceType {
-    PAWN("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-pawn.png", 1),
-    ROOK("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-rook.png", 5),
+    PAWN(
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-pawn.png",
+        1,
+        false
+    ),
+    ROOK(
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-rook.png",
+        5,
+        false
+    ),
     BISHOP(
         "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-bishop.png",
-        3
+        3,
+        false
     ),
     KNIGHT(
         "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-knight.png",
-        3
+        3,
+        false
     ),
     QUEEN(
         "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-queen.png",
-        9
+        9,
+        false
     ),
-    KING("/Users/kamraan/Desktop/Chess Engine/pieces_images/black-king.png", 0),
-    pawn("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-pawn.png", 1),
-    rook("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-rook.png", 5),
+    KING(
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/black-king.png",
+        0,
+        false
+    ),
+    pawn(
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-pawn.png",
+        1,
+        true
+    ),
+    rook(
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-rook.png",
+        5,
+        true
+    ),
     bishop(
         "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-bishop.png",
-        3
+        3,
+        true
     ),
     knight(
         "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-knight.png",
-        3
+        3,
+        true
     ),
     queen(
         "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-queen.png",
-        9
+        9,
+        true
     ),
-    king("/Users/kamraan/Desktop/Chess Engine/pieces_images/white-king.png", 0);
+    king(
+        "/Users/kamraan/Desktop/Chess Engine/pieces_images/white-king.png",
+        0,
+        true
+    );
 
     private Image pImage = null;
 
     private int value;
+    private boolean isWhite;
 
-    private PieceType(String imagePath, int value) {
+    private PieceType(String imagePath, int value, Boolean isWhite) {
         this.pImage = new ImageIcon(imagePath).getImage();
         this.value = value;
+        this.isWhite = isWhite;
     }
 
     public Image getImage() {
@@ -50,5 +82,9 @@ public enum PieceType {
 
     public int value() {
         return value;
+    }
+
+    public boolean isWhite() {
+        return isWhite;
     }
 }
